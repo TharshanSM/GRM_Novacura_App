06418dbbf70c69735aab5dbb9110390192d70e01
TharshanSM <ugatharshan95@gmail.com>
2023-01-12 08:07:46 +0530

    Pages Format Modify

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
 
 const allocationRoutes = require("./routes/allocation");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/allocations", allocationRoutes);
