fc72f13023471e07cadd11c91b39d12036b44fec
TharshanSM <ugatharshan95@gmail.com>
2023-02-13 13:24:29 +0530

    Created Add Allocation API

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,14 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 
-const allocationRoutes = require("./routes/allocation");
+const allocationRoutes = require("./routes/allocations");
+const employeeRoutes = require("./routes/employees");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/allocations", allocationRoutes);
+app.use("/employees", employeeRoutes);
 
 app.get("/", (req, res) => {
     res.send("Global Resource Management App");
